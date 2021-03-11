932a304a5cc5ed2b8c5f0f5066c61b5283bf8b3e
Ayhan <abedirhan@hotmail.com>
2021-03-10 23:06:24 -0500

    Added party controller

--- src/main/java/com/my/grocery/controler/PartyController.java
+++ src/main/java/com/my/grocery/controler/PartyController.java
@@ -1,4 +1,16 @@
 package com.my.grocery.controler;
 
+import com.my.grocery.service.PartyService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/party")
+@Api(tags = {"Party"}, description = "Party api")
 public class PartyController {
+    @Autowired
+    private PartyService partyService;
+
 }
